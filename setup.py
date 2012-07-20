94b677b104f20015c3a9d9dbd5f68508adc6f9fa
Tom Nightingale <tom@affinitybridge.com>
2012-07-19 17:51:42 -0700

    added version, hashbang

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
+
+version = open('VERSION', 'r').read().strip()
+
 setup(name='TileExpire',
-      version='0.0.1',
+      version=version,
       packages=['TileExpire'],
       py_modules = ['TileExpire.Tiler'],
       scripts=['scripts/tileexpire-serve.py', 'scripts/tileexpire-client.py'])
