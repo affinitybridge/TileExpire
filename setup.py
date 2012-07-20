c157cb5d6dd1ef172cca794b405d7c2d5c49f346
Tom Nightingale <tom@affinitybridge.com>
2012-07-19 17:54:59 -0700

    packaging metadata

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ version = open('VERSION', 'r').read().strip()
 
 setup(name='TileExpire',
       version=version,
+      description='Experimental cache manager middleware for TileStache.',
+      author='Tom Nightingale',
+      author_email='tom@affinitybridge.com',
+      url='http://affinitybridge.com',
+      requires=['ModestMaps (>=1.3.0)', 'TileStache (>=1.38.0)', 'werkzueg'],
       packages=['TileExpire'],
       py_modules = ['TileExpire.Tiler'],
       scripts=['scripts/tileexpire-serve.py', 'scripts/tileexpire-client.py'])
